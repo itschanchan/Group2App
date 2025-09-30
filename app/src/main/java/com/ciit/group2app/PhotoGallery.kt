8800efa7362418d80b847dc2d468b0ea4b16474f
NormalGuy866 <eimercanapi22@gmail.com>
2025-09-30 08:59:53 +0800

    Added ImageView and Listeners PhotoGallery,

--- app/src/main/java/com/ciit/group2app/PhotoGallery.kt
+++ app/src/main/java/com/ciit/group2app/PhotoGallery.kt
@@ -1,6 +1,8 @@
 package com.ciit.group2app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,50 @@ class PhotoGallery : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val img1 = findViewById<ImageView>(R.id.donk1)
+
+        val img2 = findViewById<ImageView>(R.id.shrek1)
+
+        val img3 = findViewById<ImageView>(R.id.shrek2)
+
+        val img4 = findViewById<ImageView>(R.id.donk2)
+
+        val img5 = findViewById<ImageView>(R.id.donk3)
+
+        val img6 = findViewById<ImageView>(R.id.shrek3)
+
+        img1.setOnClickListener {
+            val intent = Intent(this, Page1::class.java)
+            startActivity(intent)
+        }
+
+        img2.setOnClickListener {
+            val intent = Intent(this, Page2::class.java)
+            startActivity(intent)
+        }
+
+        img3.setOnClickListener {
+            val intent = Intent(this, Page3::class.java)
+            startActivity(intent)
+        }
+
+        img4.setOnClickListener {
+            val intent = Intent(this, Page4::class.java)
+            startActivity(intent)
+        }
+
+        img5.setOnClickListener {
+            val intent = Intent(this, Page5::class.java)
+            startActivity(intent)
+        }
+
+        img6.setOnClickListener {
+            val intent = Intent(this, Page6::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
